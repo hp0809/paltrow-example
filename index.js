a29225420355b7dfacd0e870b442d2d505f73db2
Hali Power <hpower0809@gmail.com>
2019-05-28 20:50:25 +0100

    inital commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import App from './paltrows-power-toes/paltrows-power-toes-1-setup/src/App';
+import './index.css';
+
+ReactDOM.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>,
+     document.getElementById('root')
+     );
